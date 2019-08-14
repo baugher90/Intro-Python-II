d016e8d4502b444b1640dccf8483b52819496e13
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-08-13 22:10:53 -0400

    added Player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def__init__(self, name,current_room):
+        self.name = name
+        self.current_room = current_room
+
+    def__str__(self):
+        s = f'Player Name:{self.name}/n Current Location:{self.current_room}'
+        return s
